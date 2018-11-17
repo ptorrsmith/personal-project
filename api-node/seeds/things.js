01858bff89ea6f672c81039bc301e0a4c5934c6d
Peter Torr Smith <peter@ptorrsmith.com>
2018-11-17 05:35:58 +0000

    Get things from DB returning via api

--- api-node/seeds/things.js
+++ api-node/seeds/things.js
@@ -2,6 +2,12 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('things').del().then(function() {
     // Inserts seed entries
-    return knex('things').insert([{ "id": 1, "name": "Trailer", "daily_rate_in_cents": 5000, "owner_member_id": 1, "quantity": 1 }, { "id": 2, "name": "Backpack", "daily_rate_in_cents": 700, "owner_member_id": 2, "quantity": 2 }, { "id": 3, "name": "Sleeping Bag", "daily_rate_in_cents": 700, "owner_member_id": 2, "quantity": 4 }, { "id": 4, "name": "Party light set", "daily_rate_in_cents": 8499, "owner_member_id": 3 }]);
+    return knex('things').insert([
+      { "id": 1, "name": "Trailer", "daily_rate_in_cents": 5000, "owner_member_id": 1, "quantity": 1 },
+      { "id": 2, "name": "Backpack", "daily_rate_in_cents": 700, "owner_member_id": 2, "quantity": 2 },
+      { "id": 3, "name": "Sleeping Bag", "daily_rate_in_cents": 700, "owner_member_id": 2, "quantity": 4 },
+      { "id": 4, "name": "Party light set", "daily_rate_in_cents": 8499, "owner_member_id": 3 },
+      { "id": 5, "name": "Outdoor sound system", "daily_rate_in_cents": 7000, "owner_member_id": 3 }
+    ]);
   });
 };
--- api-node/server/db/thingsDB.js
+++ api-node/server/db/thingsDB.js
@@ -1,69 +1,80 @@
+// EXPORTED FUNCTIONS
+
 module.exports = {
   getThings,
   getThing,
   addThing,
   updateThing,
   deleteThing
-  }
+}
 
 let things = require('./data/things')
 
+const config = require('../../knexfile').development // [environment]
+const connection = require('knex')(config)
+
 // returning promises to mock the knex asynchronous behaviour
 
-function getThings(owner_id) {
-  return new Promise( (resolve, reject) => {
-  resolve(things.filter( thing => !owner_id || thing.owner_id == owner_id ))
-  })
+function getThings(owner_id, db = connection) {
+
+  return db('things').select() //.where('owner_id', owner_id)
 }
+
+
+// return new Promise((resolve, reject) => {
+//   resolve(things.filter(thing => !owner_id || thing.owner_id == owner_id))
+// })
+// }
 
 
 function getThing(id) {
-  const thing = things.find( thing => thing.id == id)
-  return new Promise( (resolve, reject) => {
-  resolve(thing)
-});
-    
+  const thing = things.find(thing => thing.id == id)
+  return new Promise((resolve, reject) => {
+    resolve(thing)
+  });
+
 }
 
 function addThing(thing) {
-  const highestIdInArray = things.map( thing => thing.id).reduce( (highestThingId, currentThingId) => {
+  const highestIdInArray = things.map(thing => thing.id).reduce((highestThingId, currentThingId) => {
     return Math.max(highestThingId, currentThingId)
   })
   thing.id = highestIdInArray + 1
   things.push(thing)
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     resolve(thing)
   })
 }
 
 function updateThing(thing) {
   // toDo
-  const existingThing = things.find( thingItem => thingItem.id == thing.id)
-  return new Promise( (resolve, reject) => {
-    if (existingThing) {
-      const index = things.findIndex( thingItem => thingItem.id == existingThing.id)
+  const existingThing = things.find(thingItem => thingItem.id == thing.id)
+  return new Promise((resolve, reject) => {
+    if(existingThing) {
+      const index = things.findIndex(thingItem => thingItem.id == existingThing.id)
       things.slice(index, 1, thing)
       resolve(thing)
     }
   })
-  }
+}
 
 function deleteThing(id) {
   // chose to only send id through as this more like real world
   // find thing in our things array
   let result;
   const thing = things.find(thing => thing.id == id)
-  if (thing) {
+  if(thing) {
     const index = things.findIndex(thingElement => {
       return thingElement.id == thing.id
     })
     // delete from array
     things.splice(index, 1)
     result = 'Success'
-  } else {
+  }
+  else {
     result = 'Fail: no such thing to delete'
   }
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     resolve(result)
   })
 }
