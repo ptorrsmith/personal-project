57d100f72b0b129e9fc3821fa51949b594790393
oliver-harcourt <oliver.harcourt@gmail.com>
2018-12-03 14:02:41 +1300

    combiner

--- thingiverse/client/index.js
+++ thingiverse/client/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 // import ReactDOM from "react-dom";
 import { render } from "react-dom";
+import { provider } from 'react-redux'
 
 import App from './components/App'
 
 // add redux store. Give it reducers.
+
 
 document.addEventListener('DOMContentLoaded', () => {
     // ReactDOM.render(
